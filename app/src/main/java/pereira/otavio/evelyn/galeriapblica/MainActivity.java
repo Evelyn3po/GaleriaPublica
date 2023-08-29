f313383085912910e32e977affe4eccf9bb04103
20221TIIMI0351 <evelynpo59@gmail.com>
2023-08-29 09:08:05 -0300

    Ajeitar erros

--- app/src/main/java/pereira/otavio/evelyn/galeriapblica/MainActivity.java
+++ app/src/main/java/pereira/otavio/evelyn/galeriapblica/MainActivity.java
@@ -2,10 +2,15 @@ package pereira.otavio.evelyn.galeriapblica;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -32,24 +37,88 @@ public class MainActivity extends AppCompatActivity {
         //é realizado o commit da transação
         fragmentTransaction.commit();
     }
+    @Override
+    //toda vez que o usuário selecionar uma das opções, o método onNavigationItemSelected será chamado
+    public void onRequestPermissionsResult(int requestCode, @NonNull
+    String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
+        final List<String> permissionsRejected = new ArrayList<>();
+        if(requestCode == RESULT_REQUEST_PERMISSION ) {
+
+            for(String permission : permissions) {
+                if(!hasPermission(permission)) {
+                    permissionsRejected.add(permission);
+
+                }
+            }
+        }
+
+        if(permissionsRejected.size() > 0) {
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if(shouldShowRequestPermissionRationale(permissionsRejected.get(0)))
+                {
+                    new AlertDialog.Builder(MainActivity.this).
+                            setMessage("Para usar essa app é preciso conceder essas permissões").
+                                    setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                        @Override
+
+                                        public void onClick(DialogInterface
+                                                                    dialog, int which) {
+                                            requestPermissions(permissionsRejected.toArray(new
+                                                    String[permissionsRejected.size()]), RESULT_REQUEST_PERMISSION);
+                                        }
+                                    }).create().show();
+            }
+
+        }
+        else {
+            MainViewModel vm = new
+                    ViewModelProvider(this).get(MainViewModel.class);
+            int navigationOpSelected = vm.getNavigationOpSelected();
+            bottomNavigationView.setSelectedItemId(navigationOpSelected);
+        }
+    }
+    private void checkForPermissions(List<String> permissions) {
+        List<String> permissionsNotGranted = new ArrayList<>();
+
+            for(String permission : permissions) {
+                if( !hasPermission(permission)) {
+                    permissionsNotGranted.add(permission)
+                }
+            }
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if(permissionsNotGranted.size() > 0) {
+                    requestPermissions(permissionsNotGranted.toArray(new
+                            String[permissionsNotGranted.size()]),RESULT_REQUEST_PERMISSION);
+                }
+            }
+        }
+
+        private boolean hasPermission(String android.Manifest.permission) {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                return
+                        ActivityCompat.checkSelfPermission(MainActivity.this, permission) ==
+                                PackageManager.PERMISSION_GRANTED;
+            }
+            return false;
+
+        }
+
+        else {
+            MainViewModel vm = new
+                    ViewModelProvider(this).get(MainViewModel.class);
+            int navigationOpSelected = vm.getNavigationOpSelected();
+            bottomNavigationView.setSelectedItemId(navigationOpSelected);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        private void checkForPermissions(List<String> permissions) {
-            List<String> permissionsNotGranted = new ArrayList<>();
-            if(permissionsNotGranted.size() > 0) {
-
-            }
-            else {
-                MainViewModel vm = new
-                        ViewModelProvider(this).get(MainViewModel.class);
-                int navigationOpSelected = vm.getNavigationOpSelected();
-                bottomNavigationView.setSelectedItemId(navigationOpSelected);
-            }
-        }
 
         // obtemos uma referência para MainViewModel
         final MainViewModel vm = new
@@ -61,24 +130,9 @@ public class MainActivity extends AppCompatActivity {
         //setamos em bottonNavigationView o “escutador” de eventos de seleção do menu
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
 
-            @Override
-            //toda vez que o usuário selecionar uma das opções, o método onNavigationItemSelected será chamado
-            public void onRequestPermissionsResult(int requestCode, @NonNull
-            String[] permissions, @NonNull int[] grantResults) {
-                super.onRequestPermissionsResult(requestCode, permissions,
-
-                if(permissionsRejected.size() > 0) {
-
-            }
-                else {
-                    MainViewModel vm = new
-                            ViewModelProvider(this).get(MainViewModel.class);
-                    int navigationOpSelected = vm.getNavigationOpSelected();
-                    bottomNavigationView.setSelectedItemId(navigationOpSelected);
-                }
-            }
         });
     }
+
     @Override
     protected void onResume() {
         super.onResume();
